f5663dfac3a276f2c978fb4c1b60298a0b479355
boyapativennela <boyapativennela58@gmail.com>
2025-08-09 17:09:56 +0530

    updated pro

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -4,16 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ClientController {
-	@GetMapping("/")
-	public String home()
-	{
-		return "CI/CD and cloud devops";
-	}
-	@GetMapping("/demo")
-   public String demo()
-   {
-		return "jenkins demo";
-   }
+public class ClientController
+{
+  @GetMapping("/")
+  public String home()
+  {
+    return "CI/CD and Cloud DevOps";
+  }
+  @GetMapping("/demo")
+  public String demo() {
+    return "Jenkins Demo";
+  }
+  @GetMapping("/test")
+  public String test() {
+    return "Deploy Spring Boot using Jenkins";
+  }
+
 
 }
